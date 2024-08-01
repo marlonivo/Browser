c3a5d3856c859409176d6ca03315e0982c25e0e7
marlonivo <110742116+marlonivo@users.noreply.github.com>
2024-08-01 00:20:51 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,17 @@
+#!/bin/bash
+
+# Verzeichnisse erstellen, falls nicht vorhanden
+mkdir -p ~/.mozilla/firefox/*.default-release/chrome
+
+# Dateien kopieren
+cp -r chrome ~/.mozilla/firefox/*.default-release/
+cp prefs.js ~/.mozilla/firefox/*.default-release/
+cp user.js ~/.mozilla/firefox/*.default-release/
+
+# Add-ons installieren (Beispielhaft)
+firefox "https://addons.mozilla.org/firefox/downloads/file/3784780/ublock_origin-1.38.6-an+fx.xpi"
+firefox "https://addons.mozilla.org/firefox/downloads/file/3786550/vimium_c-1.92.5-an+fx.xpi"
+firefox "https://addons.mozilla.org/firefox/downloads/file/3788321/stylus-1.5.10-an+fx.xpi"
+
+echo "Installation abgeschlossen. Bitte Firefox neu starten."
 
